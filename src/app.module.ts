59649e41490b3f7cce3325fc497c7c7d64c26016
Mauricio Zepeda <mauro.zepeda.rivera@gmail.com>
2022-10-16 15:44:40 -0300

    pokedex init

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common'; 
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+    }),
+    CommonModule,
+    PokemonModule, 
+    MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
+
+
+ 
